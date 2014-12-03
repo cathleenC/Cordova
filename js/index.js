6736baa78ff38c7f1b51211db6f58ae9607f63e3
cathleenC <cathleen.clop@outlook.fr>
2014-12-03 10:43:48 +0200

    un

---
+++ js/index.js
@@ -0,0 +1,140 @@
+var allqueues = {
+    "lesqueues": [
+        {
+            "nom": "Magasin",
+            "url": "#magasin"
+        },
+        {  
+            "nom": "Administration",
+            "url": "#administration"
+        },
+        
+            "nom": "Loisir",
+            "url": "#loisir"
+        }
+    ]
+}
+
+var queues = {
+    "lesqueues": [
+        {
+            "nom": "Magasin",
+            "url": "#magasin"
+        },
+        {  
+            "nom": "Administration",
+            "url": "#administration"
+        },
+        
+            "nom": "Loisir",
+            "url": "#loisir"
+        }
+    ]
+}
+
+//photo
+$(document).ready(function() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+    //for testing in Chrome browser uncomment
+    //onDeviceReady();
+});
+ 
+var pictureSource;
+var destinationType;
+ 
+function onDeviceReady() {
+    pictureSource = navigator.camera.PictureSourceType;
+    destinationType = navigator.camera.DestinationType;
+    navigator.geolocation.getCurrentPosition(onSuccess, onError, {maximumAge: 10000, timeout: 300000, enableHighAccuracy: true});
+}
+ 
+function capturePhoto() {
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, {quality: 50});
+}
+ 
+function onPhotoDataSuccess(imageData) {
+    var smallImage = document.getElementById('image');
+    smallImage.style.display = 'block';
+    smallImage.src = imageData;
+}
+ 
+function onFail(message) {
+    alert('Failed to load picture because: ' + message);
+}
+
+//GPS
+function onSuccess(position) {
+    var element = document.getElementById('geolocation');
+        element.innerHTML =
+        'Latitude:          '+ position.coords.latitude+'<br/>'+
+        'Longitude:         '+ position.coords.longitude+'<br/>'+
+        'Altitude:          '+ position.coords.altitude+'<br/>'+
+        'Accuracy:          '+ position.coords.accuracy+'<br/>'+
+        'Altitude Accuracy: '+ position.coords.altitudeAccuracy+'<br/>'+
+        'Heading:           '+ position.coords.heading+'<br/>'+
+        'Speed:             '+ position.coords.speed+'<br/>'+
+        'Timestamp:         '+ position.timestamp+'<br/>';
+}
+ 
+function onError() {
+    var element = document.getElementById('geolocation');
+    element.innerHTML = 'Position inconnue';
+}
+
+
+
+window.addEventListener('load', function () {
+    new FastClick(document.body);
+}, false);
+
+$(window).on('hashchange', route);
+ 
+function route() {
+    var page, hash = window.location.hash;
+    switch (hash) {
+        case "#profil":
+            $.get('js/templates.html', function(templates) {
+                var page = $(templates).filter('#tpl-profil').html();
+                $('#container').html(page); // with jQuery
+            }, 'html');
+            break;
+
+         case "#allqueues":
+            $.get('js/templates.html', function(templates) {
+                var template = $(templates).filter('#tpl-queues-allqueues').html();
+                page = Mustache.render(template, allqueues);
+                document.getElementById("container").innerHTML = page;
+            }, 'html');
+            break; 
+
+         case "#queues":
+            $.get('js/templates.html', function(templates) {
+                var template = $(templates).filter('#tpl-queues-allqueues').html();
+                page = Mustache.render(template, queues);
+                document.getElementById("container").innerHTML = page;
+            }, 'html');
+            break;
+
+        case "#aqueue":
+            $.get('js/templates.html', function(templates) {
+                var page = $(templates).filter('#tpl-aqueue').html();
+                $('#container').html(page); // with jQuery
+            }, 'html');
+            break;
+ 
+        default:
+            $.get("js/templates.html", function(templates) {
+                page = $(templates).filter('#tpl-home').html();
+                document.getElementById("container").innerHTML = page;
+            }, 'html');
+            break;
+    }
+}
+
+
+
+
+route();
+
+
+
